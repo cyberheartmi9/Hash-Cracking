500e790ccb212a0e064118918e4fc18987d781ca
PikaChu <abdelazimmohmmed@gmail.com>
2017-08-25 00:24:16 +0300

    Update CrackHash.py

--- CrackHash.py
+++ CrackHash.py
@@ -6,15 +6,47 @@ import  signal
 
 
 print """
-        ##################################################
-        ##                                              ##
-        ##                                              ##
-        ##                                              ##
-        ##   Author @intx0x80                           ##
-        ##                                              ##
-        ##      Hash Cracking                           ##
-        ##                                              ##
-        ##################################################
+
+
+                                                                            
+                                                                            
+HHHHHHHHH     HHHHHHHHH                                 hhhhhhh             
+H:::::::H     H:::::::H                                 h:::::h             
+H:::::::H     H:::::::H                                 h:::::h             
+HH::::::H     H::::::HH                                 h:::::h             
+  H:::::H     H:::::H    aaaaaaaaaaaaa      ssssssssss   h::::h hhhhh       
+  H:::::H     H:::::H    a::::::::::::a   ss::::::::::s  h::::hh:::::hhh    
+  H::::::HHHHH::::::H    aaaaaaaaa:::::ass:::::::::::::s h::::::::::::::hh  
+  H:::::::::::::::::H             a::::as::::::ssss:::::sh:::::::hhh::::::h 
+  H:::::::::::::::::H      aaaaaaa:::::a s:::::s  ssssss h::::::h   h::::::h
+  H::::::HHHHH::::::H    aa::::::::::::a   s::::::s      h:::::h     h:::::h
+  H:::::H     H:::::H   a::::aaaa::::::a      s::::::s   h:::::h     h:::::h
+  H:::::H     H:::::H  a::::a    a:::::assssss   s:::::s h:::::h     h:::::h
+HH::::::H     H::::::HHa::::a    a:::::as:::::ssss::::::sh:::::h     h:::::h
+H:::::::H     H:::::::Ha:::::aaaa::::::as::::::::::::::s h:::::h     h:::::h
+H:::::::H     H:::::::H a::::::::::aa:::as:::::::::::ss  h:::::h     h:::::h
+HHHHHHHHH     HHHHHHHHH  aaaaaaaaaa  aaaa sssssssssss    hhhhhhh     hhhhhhh
+                                                                            
+                                                                            
+                                                                            
+                                                                            
+                                                                            
+            
+                      _             
+                     | |            
+   ___ _ __ __ _  ___| | _____ _ __ 
+  / __| '__/ _` |/ __| |/ / _ \ '__|
+ | (__| | | (_| | (__|   <  __/ |   
+  \___|_|  \__,_|\___|_|\_\___|_|   
+                                    
+                                    
+                                                                
+                                                                            
+
+
+
+Author @intx0x80                           
+        
 """
 
 def signal_handler(signal, frame):
